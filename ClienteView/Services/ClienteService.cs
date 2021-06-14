b4f820a80830ecd95da2adbc17d15c33e0843127
Brian Alexander Gomez <bgomez@softcorpapp.com>
2021-06-14 16:02:46 -0400

    Metodo de crear

--- ClienteView/Services/ClienteService.cs
+++ ClienteView/Services/ClienteService.cs
@@ -6,6 +6,7 @@ using System.Configuration;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Text;
 using System.Threading.Tasks;
 using System.Web;
 
@@ -70,6 +71,31 @@ namespace ClienteView.Services
                     Message = e.Message,
                     Data = null
                 };
+            }
+        }
+
+        public async Task<OperationResult<ClienteModel>> CreateCliente(ClienteModel cliente) 
+        {
+            HttpResponseMessage response;
+            try
+            {
+                var jsontext = JsonConvert.SerializeObject(cliente);
+                var content = new StringContent(jsontext, Encoding.UTF8, "application/json");
+
+                response = await Http.PostAsync($"{BaseUrl}/api/cliente",content);
+                response.EnsureSuccessStatusCode();
+                OperationResult<ClienteModel> responseBody = JsonConvert.DeserializeObject<OperationResult<ClienteModel>>(await response.Content.ReadAsStringAsync());
+                return responseBody;
+            }
+            catch (Exception e)
+            {
+                return new OperationResult<ClienteModel>()
+                {
+                    Data = null,
+                    Status = (int)HttpStatusCode.BadRequest,
+                    Message = e.Message
+                };
+                throw;
             }
         }
     }
